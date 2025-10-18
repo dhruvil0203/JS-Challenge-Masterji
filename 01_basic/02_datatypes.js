8e38c0d20d85a04f71712bf6e9bcf5568ff86b80
Dhruvil Mistry <dkmistry0203@gmail.com>
2025-10-18 09:10:28 +0000

    datatypes and typeconversion

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+// alert ("Hello World")
+
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined
+
+// Object
+console.log(typeof "");
+console.log(typeof undefined);
+console.log(typeof null);
+
+
---
+++ 01_basic/03_typeconversion.js
@@ -0,0 +1,11 @@
+let score = 88
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let isLoggedIn = 0;
+let isLoggedInString = "abc";
+
+let booleanIsLoggedIn = Boolean(isLoggedInString)
+
+console.log(booleanIsLoggedIn);
